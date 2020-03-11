716e97cd3f7da337c1d6a77c3f058d1792e4d548
scalatar <jamie.mcmurray@scalar.ca>
2020-03-11 11:37:22 -0700

    Update example

--- ansible/start_splunk_deployment.sh
+++ ansible/start_splunk_deployment.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-ansible-playbook -i inventory playbooks/install-splunk.yml
+
+# standalone
+SPLUNK_DEPLOYMENT_ROLES="splunk_standalone" ansible-playbook -i inventory/inventory.py playbooks/install-standalone.yml
+
+# cluster example
+#SPLUNK_DEPLOYMENT_ROLES="splunk_multisite:splunk_indexer:splunk_cluster_master:splunk_search_head:splunk_search_head_captain:splunk_deployer:splunk_license_master:splunk_deployment_server" ansible-playbook -i inventory/inventory.py playbooks/install-clustered.yml
