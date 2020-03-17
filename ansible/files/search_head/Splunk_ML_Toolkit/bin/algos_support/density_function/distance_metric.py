d06081c747274a1857f03cfe19934e802a2baabb
scalatar <jamie.mcmurray@scalar.ca>
2020-03-17 13:30:10 -0700

    Remove unused app

--- ansible/files/search_head/Splunk_ML_Toolkit/bin/algos_support/density_function/distance_metric.py
+++
@@ -1,7 +0,0 @@
-class DistanceMetric(object):
-    """Enum class to enumerate different
-    distance measures that we support
-    """
-
-    KOLMOGOROV_SMIRNOV = 'kolmogorov_smirnov'
-    WASSERSTEIN = 'wasserstein'
--- ansible/files/search_head/Splunk_ML_Toolkit/bin/scorings/regression/MeanSquaredError.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-
-from base_scoring import BaseScoring, RegressionScoringMixin
-
-
-class MeanSquaredErrorScoring(RegressionScoringMixin, BaseScoring):
-    """Implements sklearn.metrics.mean_squared_error"""
